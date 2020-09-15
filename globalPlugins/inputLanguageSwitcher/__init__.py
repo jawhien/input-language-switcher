6e13913e166e6579d9d99f4dad409f33d3c6b403
jawhien <jawhien@yandex.by>
2020-09-15 18:58:42 +0300

    Added first version.

---
+++ globalPlugins/inputLanguageSwitcher/__init__.py
@@ -0,0 +1,13 @@
+
+from scriptHandler import script
+import globalPluginHandler
+import ctypes
+
+class GlobalPlugin(globalPluginHandler.GlobalPlugin):
+
+	@script(description="Switch input language", gesture="kb:windows+space")
+	def script_reportMouseObject(self, gesture):
+		user32 = ctypes.windll.user32
+
+		hnd=user32.GetForegroundWindow()
+		user32.PostMessageA(hnd, 0x0050, 2, 0)
