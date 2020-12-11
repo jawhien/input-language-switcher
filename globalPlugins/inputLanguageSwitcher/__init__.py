01058eb8583e26c7b570f76f13cfa73aa62f58fe
jawhien <jawhien@yandex.by>
2020-12-11 22:48:11 +0300

    New version 1.0

--- globalPlugins/inputLanguageSwitcher/__init__.py
+++ globalPlugins/inputLanguageSwitcher/__init__.py
@@ -1,14 +1,13 @@
 
-from scriptHandler import script
 import globalPluginHandler
 import ctypes
 import api
+from scriptHandler import script
 
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 
 	@script(description="Switch input language", gesture="kb:windows+space")
-	def script_langSwitch(self, gesture):
+	def script_switchLanguage(self, gesture):
 		user32 = ctypes.windll.user32
-
 		hnd=api.getFocusObject().windowHandle
 		user32.PostMessageA(hnd, 0x0050, 2, 0)
